92a1f46218c81c19d411fe12f12f6730c0dc2f04
matiraw <matisierra.develop@gmail.com>
2022-01-21 16:51:44 -0300

    Asercion de tipos

--- index.ts
+++ index.ts
@@ -65,3 +65,17 @@ console.log(employeestatus)
 
 //Para mostrar el nombre asociado a la enumeración, podemos usar el indexador proporcionado.
 console.log(ContractStatus[employeestatus]);
+
+
+//asercion de tipos
+// Aserción de tipos
+
+// Si necesita tratar una variable como un tipo de datos diferente, puede usar una aserción de tipos. Una aserción de tipos indica a TypeScript que ha realizado cualquier comprobación especial que necesite antes de llamar a la instrucción. Indica al compilador "confía en mí, sé lo que estoy haciendo". Una aserción de tipos es como una conversión de tipos en otros lenguajes, pero no realiza ninguna comprobación especial ni reestructuración de los datos. No tiene ningún impacto en el tiempo de ejecución y lo utiliza exclusivamente el compilador.
+
+// Las aserciones de tipos tienen dos formatos. Una es la sintaxis de as:
+
+// (randomValue as string).toUpperCase();
+
+// La otra versión es la sintaxis de "corchetes angulares":
+
+// (<string>randomValue).toUpperCase();
