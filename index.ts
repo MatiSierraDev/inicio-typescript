23b978c349fe2d4ab59907b8ff26c5145e4ff08d
matiraw <matisierra.develop@gmail.com>
2022-01-21 15:53:40 -0300

    Agregue nueva rama 'tipos-de-variable' para probar los tipos de typescript

--- index.ts
+++ index.ts
@@ -2,3 +2,30 @@ function addNumbers(x: number, y: number) {
   return x + y;
 }
 console.log(addNumbers(3, 6));
+
+//tipo number
+let numero: number;
+
+let numeroSintax: number = 10;
+
+let x = 10;
+
+let y;
+
+
+// x = 'un string'; no acepta un valor que no sea numerico
+
+numero = 32;
+
+
+//Tipo Booleano
+let soyUnBooleano: boolean;
+let yes = true;
+let not = false;
+
+
+//Tipos numéricos y BigInteger
+let number : number;
+let valor = 0;
+let z: number = 123.456;
+// let big: bigint = 100n;
